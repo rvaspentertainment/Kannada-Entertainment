9b7719c811f16c6808ec2f19d15bf36fee51952e
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-09-28 21:30:59 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,11 @@
+# bot/__init__.py
+"""
+Kannada Entertainment Bot Module
+
+This module contains all the bot functionality organized into different handlers.
+"""
+
+from .handlers import *  # Import all handlers
+
+__version__ = "1.0.0"
+__author__ = "Kannada Entertainment Team"
